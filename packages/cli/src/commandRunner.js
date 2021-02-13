204655aa61ecac6ffab84cacf73c5764e0def500
Joseph <joseph@titaniumcoast.com>
2021-02-12 23:11:11 -0500

    Framework
    This likely will work, but only testing can find that out.

--- packages/cli/src/commandRunner.js
+++ packages/cli/src/commandRunner.js
@@ -1,11 +1,34 @@
+#!/usr/bin/env node
+
+const processFile = require("./processFile.js");
+
+const glob = require("glob");
 const { Command } = require("commander");
 const program = new Command();
 program.version("0.0.1");
 
 program
-  .option("-s, --source <location>", "Changes the source location.", "src")
-  .option("-o, --output <location>", "Changes the output location.", "lib");
+  .option("-s, --source <directory>", "Changes the source directory.", "src")
+  .option("-o, --output <directory>", "Changes the output directory.", "lib");
 
 program.parse(process.argv);
 
-// const opts = program.opts();
+const opts = program.opts();
+
+glob(opts.source+"/**.😀", {}, (err, files) => {
+    if(err) {
+        throw new Error(err);
+    }
+    files.forEach((file) => {
+        processFile(opts.source, file, opts.output);
+    });
+});
+
+glob(opts.source+"/**.smiley", {}, (err, files) => {
+    if(err) {
+        throw new Error(err);
+    }
+    files.forEach((file) => {
+        processFile(opts.source, file, opts.output);
+    });
+});
