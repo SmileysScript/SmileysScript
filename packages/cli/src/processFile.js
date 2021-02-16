bc08e73fccb8d421076004382aa83f14888e4c74
JRed <joseph@titaniumcoast.com>
2021-02-15 22:03:01 -0500

    Fixed Dir Issue

--- packages/cli/src/processFile.js
+++ packages/cli/src/processFile.js
@@ -6,13 +6,24 @@ const processFile = (pathIn, file, pathOut) => {
   try {
     const code = fs.readFileSync(file, "utf8");
     const output = smileysscript.compile(code);
+    const filePath = path.resolve(pathOut, file.slice(0, -path.extname(file).length) + ".js");
+    ensureDirectoryExistence(filePath);
     fs.writeFileSync(
-      path.resolve(pathOut, file.slice(0, -path.extname(file).length) + ".js"),
+      path.resolve(filePath),
       output
     );
   } catch (err) {
     throw new Error(err);
   }
 };
+
+const ensureDirectoryExistence = (filePath) => {
+    const dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
 
 module.exports = processFile;
