3a3d656e47ef637984363cc3f2f762c53e8711a6
JRed <joseph@titaniumcoast.com>
2021-02-16 18:22:19 -0500

    Hopefully this fixes the latest issue

--- packages/cli/src/processFile.js
+++ packages/cli/src/processFile.js
@@ -8,7 +8,7 @@ const processFile = (pathIn, file, pathOut) => {
     const output = smileysscript.compile(code);
     const fileSplit = file.split(path.sep);
     const pathInSplit = pathIn.split(path.sep);
-    const finalFile = path.resolve(shiftMulti(pathInSplit.length, fileSplit));
+    const finalFile = joinMulti(shiftMulti(pathInSplit.length, fileSplit));
     const filePath = path.resolve(
       pathOut,
       finalFile.slice(0, -path.extname(file).length) + ".js"
@@ -34,6 +34,19 @@ const shiftMulti = (times, array) => {
     array.shift();
   }
   return array;
+};
+
+const joinMulti = (array) => {
+  if (array.length === 1) {
+    return path.resolve(array);
+  } else if (array.length > 1) {
+    for (let i = 0; i < array.length - 1; i++) {
+      path.resolve(array[i], array[i + 1]);
+    }
+    return array;
+  } else {
+    return "";
+  }
 };
 
 module.exports = processFile;
